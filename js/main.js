2bd2a93c0a61614be1a7cfcac16160a34ded76bc
unknown <armanhassan504@gmail.com>
2020-03-05 15:07:00 +0600

    up-2

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//  typeit  js
+var typed = new Typed('.typeit', {
+  // Waits 1000ms after typing "First"
+  strings: ['DESIGNER',
+            'DEVELOPER',
+          ], loop: true,
+             typeSpeed: 150,
+             backSpeed: 110,
+             backDelay: 2000,
+             smartBackspace: true,
+             fadeOut: true,
+             fadeOutClass: 'typed-fade-out',
+             fadeOutDelay: 500,
+  });
